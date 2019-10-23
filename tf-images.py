b3335027bb67075b7198e22b964a496226622878
trueoleg <trueolegzaichkin@gmail.com>
2019-10-23 11:52:32 +0300

    completed tf tutorial with images and save model

--- tf-images.py
+++ tf-images.py
@@ -4,6 +4,8 @@ from tensorflow import keras
 
 import numpy as np
 import matplotlib.pyplot as plt
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 fashion_mnist = keras.datasets.fashion_mnist
 
@@ -53,14 +55,86 @@ model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
-model.fit(train_images, train_labels, epochs=10)
+model.fit(train_images, train_labels, epochs=5)
 
 test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
 
-print('\nТочность на проверочных данных:', test_acc)
+print('\nThe accuracy on the test data:', test_acc)
 
 predictions = model.predict(test_images)
 
 print(predictions[0])
 
 print(np.argmax(predictions[0]))
+
+
+def plot_image(i, predictions_array, true_label, img):
+    predictions_array, true_label, img = predictions_array[i], true_label[i], img[i]
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+
+    plt.imshow(img, cmap=plt.cm.binary)
+
+    predicted_label = np.argmax(predictions_array)
+    if predicted_label == true_label:
+        color = 'blue'
+    else:
+        color = 'red'
+
+    plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
+                                         100*np.max(predictions_array),
+                                         class_names[true_label]),
+               color=color)
+
+
+def plot_value_array(i, predictions_array, true_label):
+    predictions_array, true_label = predictions_array[i], true_label[i]
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+    thisplot = plt.bar(range(10), predictions_array, color="#777777")
+    plt.ylim([0, 1])
+    predicted_label = np.argmax(predictions_array)
+
+    thisplot[predicted_label].set_color('red')
+    thisplot[true_label].set_color('blue')
+
+
+# Displays the first X test images, their predicted and real labels.
+# Correct predictions are painted in blue, erroneous in red.
+num_rows = 5
+num_cols = 3
+num_images = num_rows*num_cols
+plt.figure(figsize=(2*2*num_cols, 2*num_rows))
+for i in range(num_images):
+    plt.subplot(num_rows, 2*num_cols, 2*i+1)
+    plot_image(i, predictions, test_labels, test_images)
+    plt.subplot(num_rows, 2*num_cols, 2*i+2)
+    plot_value_array(i, predictions, test_labels)
+plt.show()
+
+# We take one picture from the test set.
+
+img = test_images[0]
+
+print(img.shape)
+
+# Add an image to a data package that consists of only one item.
+
+img = (np.expand_dims(img, 0))
+
+print(img.shape)
+
+predictions_single = model.predict(img)
+
+print(predictions_single)
+
+plot_value_array(0, predictions_single, test_labels)
+_ = plt.xticks(range(10), class_names, rotation=45)
+
+print(np.argmax(predictions_single[0]))
+
+model.save_weights('./checkpoints/my_checkpoint')
+
+model.save('my_model.h5')
